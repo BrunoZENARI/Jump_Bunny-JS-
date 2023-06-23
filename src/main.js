af848394d33e15799a1ec33601947eaedb2c26a4
Binzen <129286646+BrunoZENARI@users.noreply.github.com>
2023-06-23 18:12:14 +0200

    version final

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@ import Phaser from "./lib/phaser.js"
 
 import Game from "./scenes/Game.js"
 
+import GameOver from './scenes/GameOver.js'
+
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 480,
     height: 640,
-    scene: Game,
+    scene: [Game, GameOver],
     physics: {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 300
+                y: 1000
             },
-            debug: true
+            debug: false
         }
     }
 })
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,6 +1,10 @@
 import Phaser from "../lib/phaser.js"
 
+import Carrot from "../game/Carrot.js"
+
 export default class Game extends Phaser.Scene{
+    carrotsCollected = 0
+
     /** @type {Phaser.Physics.Arcade.StaticGroup} */
     platforms
 
@@ -10,18 +14,35 @@ export default class Game extends Phaser.Scene{
     /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
     cursors
 
+    /** @type {Phaser.Physics.Arcade.Group} */
+    carrots
+
+    /** @type {Phaser.GameObjects.Text} */
+    carrotsCollectedText
+
     constructor() {
         super('game')
+    }
+
+    init() {
+        this.carrotsCollected = 0
     }
 
     preload(){
         this.load.image('background', 'assets/bg_layer1.png')
 
-        // load the platform imahe
+        // load the platform image
         this.load.image('platform', 'assets/ground_grass.png')
 
         // load the player
         this.load.image('bunny-stand', 'assets/bunny1_stand.png')
+
+        this.load.image('bunny-jump', 'assets/bunny1_jump.png')
+
+        // load the carrot image
+        this.load.image('carrot', 'assets/carrot.png')
+
+        this.load.audio('jump', 'assets/sfx/phaseJump1.ogg')
 
         this.cursors = this.input.keyboard.createCursorKeys()
     }
@@ -31,7 +52,7 @@ export default class Game extends Phaser.Scene{
 
         this.platforms = this.physics.add.staticGroup()
         
-        for (let i = 0; i < 6; i++){
+        for (let i = 0; i < 5; i++){
             const x = Phaser.Math.Between(80,400)
             const y = 150 * i
 
@@ -47,40 +68,68 @@ export default class Game extends Phaser.Scene{
 
         // change to use class property this.player
         this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5)
+        this.carrots = this.physics.add.group({
+            classType : Carrot
+        })
+        // this.carrots.get(240, 320, 'carrot')
 
         // same thing here in the second parameter
         this.physics.add.collider(this.platforms, this.player)
+        this.physics.add.collider(this.platforms, this.carrots)
+        
+        this.physics.add.overlap(
+            this.player,
+            this.carrots,
+            this.handleCollectCarrot,
+            undefined,
+            this
+        )
 
         this.player.body.checkCollision.up = false
         this.player.body.checkCollision.left = false
         this.player.body.checkCollision.right = false
 
         this.cameras.main.startFollow(this.player)
-
         this.cameras.main.setDeadzone(this.scale.width * 1.5)
 
+        const style = { color: '#000', fontSize: 24}
+        this.carrotsCollectedText = this.add.text(240, 10, 'Carrots: 0', style)
+            .setScrollFactor(0)
+            .setOrigin(0.5,0)
         
     }
 
     update(){
-        this.platforms.children.iterate(child => {
-            /** @type {Phaser.Physics.Arcade.Sprite} */
-            const platform = child
-
-            const scrollY = this.cameras.main.scrollY
-            if (platform.y >= scrollY + 900) {
-                platform.y = scrollY - Phaser.Math.Between(50, 100)
-                platform.body.updateFromGameObject()
-            }
-        })
-
         // find out from Arcade Physics if the player's physic body is touching something below it
         const touchingDown = this.player.body.touching.down
 
         if (touchingDown) {
             // this make the bunny jump straight up
-            this.player.setVelocityY(-400)
+            this.player.setVelocityY(-800)
+
+            this.player.setTexture('bunny-jump')
+
+            this.sound.play('jump')
         }
+
+        const vy = this.player.body.velocity.y
+        if (vy > 0 && this.player.texture.key !== 'bunny-stand') {
+            this.player.setTexture('bunny-stand')
+        }
+
+        this.platforms.children.iterate(child => {
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const platform = child
+
+            const scrollY = this.cameras.main.scrollY
+            if (platform.y >= scrollY + 700) {
+                platform.y = scrollY - Phaser.Math.Between(50, 100)
+                platform.body.updateFromGameObject()
+
+                //create a carrot above the platform being reused
+                this.addCarrotAbove(platform)
+            }
+        })
 
         // left and right input logic
         if(this.cursors.left.isDown && !touchingDown) {
@@ -93,7 +142,12 @@ export default class Game extends Phaser.Scene{
         }
 
         this.horizontalWrap(this.player)
+
+        const bottomPlatform = this.findBottomMostPlatform()
+        if (this.player.y > bottomPlatform.y +700) {
+            this.scene.start('game-over')
         }
+    }
 
         horizontalWrap(sprite){
             const halfWidth = sprite.displayWidth * 0.5
@@ -105,4 +159,61 @@ export default class Game extends Phaser.Scene{
                 sprite.x = -halfWidth
             }
         }
-    }
+
+        /** 
+         * @param {Phaser.GameObjects.Sprite} sprite
+         */
+        addCarrotAbove(sprite) {
+            const y = sprite.y - sprite.displayHeight
+
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const carrot = this.carrots.get(sprite.x, y, 'carrot')
+
+            // set active and visible
+            carrot.setActive(true)
+            carrot.setVisible(true)
+
+            this.add.existing(carrot)
+
+            // update the physics body size
+            carrot.body.setSize(carrot.width, carrot.height)
+
+            this.physics.world.enable(carrot)
+
+            return carrot
+        }
+        /** 
+        * @param {Phaser.Physics.Arcade.Sprite} player
+        * @param {Carrot} carrot
+        */
+        handleCollectCarrot(player, carrot) {
+            // hide from display
+                this.carrots.killAndHide(carrot)
+        
+            // disable from physics world
+                this.physics.world.disableBody(carrot.body)
+
+            // increment by 1
+                this.carrotsCollected++
+
+            const value = `Carrots: ${this.carrotsCollected}`
+            this.carrotsCollectedText.text = value
+       }
+
+       findBottomMostPlatform(){
+        const platforms = this.platforms.getChildren()
+        let bottomPlatform = platforms[0]
+
+        for (let i = 1; i < platforms.length; ++i) {
+            const platform = platforms[i]
+
+            if (platform.y < bottomPlatform.y) {
+                continue
+            }
+                bottomPlatform = platform
+            }
+            return bottomPlatform
+        }
+    
+
+}
